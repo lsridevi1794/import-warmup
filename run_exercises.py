6d4342474437ffd33377f2765398af0a4c4a1f37
Lakshmi Sridevi <48228252+lsridevi1794@users.noreply.github.com>
2026-04-17 16:04:16 +0000

    Warm up lab exercise 1 - Basic imports

--- run_exercises.py
+++ run_exercises.py
@@ -1,3 +1,4 @@
+import portfolio.data, portfolio.report
 if __name__ == "__main__":
     my_portfolio = portfolio.data.create_portfolio("Retirement")
     portfolio.report.print_report(my_portfolio)
